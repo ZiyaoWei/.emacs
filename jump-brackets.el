e0b7824ec81cc054b34f7939d3aa12bfc969399b
ZiyaoWei <ziyao.wei.wzy@gmail.com>
2013-11-20 20:54:29 -0500

    Delete the useless option in the jump-brackets while fixing it

--- jump-brackets.el
+++ jump-brackets.el
@@ -1,9 +1,8 @@
-(defun jump-brackets (&optional ascend-mode)
+(defun jump-brackets ()
   "Jump to the corresponding (open/close) bracket if at a close/open bracket,
    otherwise to nearest open bracket on the same level in terms of brackets."
-  (interactive "^p")
-  (or ascend-mode (setq ascend-mode 1))
+  (interactive "^")
   (let (c open)
     (setq c (following-char))
-    (cond ((member c '(?\( ?[ ?()) (goto-char (or (- scan-lists (point) 1 0) ascend-mode) (buffer-end 1))))
-           (t (goto-char (or (scan-lists (point) -1 1) (buffer-end -1))))))))
+    (cond ((member c '(?\( ?[ ?{)) (goto-char (or (- (scan-lists (point) 1 0) 1) (buffer-end 1))))
+           (t (goto-char (or (scan-lists (point) -1 1) (buffer-end -1)))))))
