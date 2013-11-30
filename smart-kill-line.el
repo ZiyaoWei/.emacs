2565050465c959820e8e3d2ae28dc12e95c2a743
ZiyaoWei <ziyao.wei.wzy@gmail.com>
2013-11-30 00:03:19 -0500

    Change the smart-kill-line to behave better

--- smart-kill-line.el
+++ smart-kill-line.el
@@ -1,6 +1,11 @@
 (defun smart-kill-line ()
-  "Kill line include the newline if currently at beginning of line. Otherwise kill-line."
+  "Kill whole line if only spaces are between the beginnning of line and point, indent the line and set the point to the line beginning position for the ease of kill again. Otherwise kill-line."
   (interactive "^")
+  (setq ctr-spaces-skipped (skip-chars-backward " "))
   (if (= (line-beginning-position) (point))
-      (kill-whole-line)
-    (kill-line)))
+      (progn
+        (kill-whole-line)
+        (beginning-of-line))
+    (progn
+      (kill-line)
+      (insert-char ?  (- ctr-spaces-skipped)))))
